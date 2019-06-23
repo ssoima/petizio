8fef5d8d22d70dc7e46151ca26b1d408437cea9c
Your Name <you@example.com>
2019-06-23 07:05:30 +0200

    updated link on click

--- web-app/src/app/petition-card/petition-card.component.ts
+++ web-app/src/app/petition-card/petition-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Inject} from '@angular/core';
 import { Petition } from '../org.petiziochain';
 import { DataService } from '../data.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'petizio-petition-card',
@@ -15,7 +16,7 @@ The component for displaying individual petition cards in the main page
 export class PetitionCardComponent implements OnInit {
   @Input() petition: Petition;
   constructor(private _route: ActivatedRoute,
-    private _router: Router, private _DataService: DataService) { 
+    private _router: Router, private _DataService: DataService, @Inject(DOCUMENT) private document: any) {
   }
 
   ngOnInit() {
@@ -23,7 +24,7 @@ export class PetitionCardComponent implements OnInit {
   }
 
   goToPetition(id) {
-    this._router.navigateByUrl('/petition/' + id);
+    this.document.location.href = '/petition/' + id;
   }
 
 }
